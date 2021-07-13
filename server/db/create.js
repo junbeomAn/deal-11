8270e14f33243a9f98e30061e913ce7a44a256ca
junbeoman <wnsqja8744@gmail.com>
2021-07-13 14:56:06 +0900

    add signin router on server and exported con object from db/create.js
    db connection을 담고 있는 con 객체를 db/create.js 에서 가져와서 signin 라우터로 요청이 들어오면
    데이터가 있는지 확인하고 세션을 이용한 로그인 처리를 한다. utils.js는 빈 파일이지만, 중복되는 db 요청에 대한 함수를 저장해볼 예정이다.

--- server/db/create.js
+++ server/db/create.js
@@ -1,21 +1,22 @@
-
 const mysql = require('mysql2');
 const config = require('./config');
-const env = process.env.NODE_ENV || "development";
-const dbname = config[env].database
+const env = process.env.NODE_ENV || 'development';
+const dbname = config[env].database;
 
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "0rladlrtmd"
+  host: 'localhost',
+  user: 'root',
+  password: '0rladlrtmd',
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
-  console.log("Connected!");
+  console.log('Connected!');
   con.query(`CREATE DATABASE ${dbname}`, function (err, result) {
     if (err) throw err;
-    console.log("Database created");
+    console.log('Database created');
     process.exit();
   });
 });
+
+module.exports = con;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,28 +1,28 @@
-var express = require('express');
-const User = require('../models/User');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const con = require('../db/create');
 
 /* GET users listing. */
-router.post('/signin', function(req, res, next) {
+router.post('/signin', function (req, res, next) {
   const { username } = req.body;
-  User.findOne({
-    where: { username }
-  })
-  .then(user => {
-    if (user.length) {
-      req.session.isLogin = true;
-      res.json({ message: "로그인이 완료되었습니다. "});
-    } else {
-      res.status(401).json({message: "로그인 실패"});
-    }
-  })
-  .catch(err => {
-    res.status(500).json({message: "server error"});
-  })
+  const query = `SELECT * FROM USERS WHERE USERNAME = ?`;
+
+  con
+    .execute(query, [username])
+    .then(([results, fields]) => {
+      console.log(results);
+      if (results.length) {
+        req.session.isLogin = true;
+        res.json({ message: '로그인이 완료되었습니다.' });
+      } else {
+        res.status(401).json({ message: '로그인 실패', ok: false });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'Server Error', ok: false });
+    });
 });
 
-router.post('/signup', function(req, res, next) {
-  
-});
+router.post('/signup', function (req, res, next) {});
 
 module.exports = router;
