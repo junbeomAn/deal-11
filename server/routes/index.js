63a81f0f5a07307daf66194779957c9ab9d60f46
junbeoman <wnsqja8744@gmail.com>
2021-07-14 10:40:56 +0900

    add: add product router to routes/index.js to separate router and add image_url field to Product model
    product 라우터를 분리하기 위해 index.js 수정 및 DB에 이미지 url을 저장하기 위한 필드 추가(image_url)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const productRouter = require('./product/index.js');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.send('index');
 });
+
+router.use('/product', productRouter);
 
 module.exports = router;
