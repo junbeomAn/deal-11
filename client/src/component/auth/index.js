067a87ad632a394c8a07560dd2035d812884e586
junbeoman <wnsqja8744@gmail.com>
2021-07-19 18:54:23 +0900

    add: add SignIn, SignUp, Default Component (Auth) with styles and utils.js
    이전에 MyInfo로 얘기하던 컴포넌트 추가. 데이터 통신부분도 포함. 추후에 일부는 XHR 로 바꿀예정

--- client/src/component/auth/index.js
+++ client/src/component/auth/index.js
@@ -1,8 +1,23 @@
 import Component from '../../core/Component';
-import SignIn from './SignIn';
 import NavBar from '../shared/NavBar';
+import Button from '../shared/Button';
+import { BASE_URL } from '../../utils';
 
 import '../../scss/auth.scss';
+import { $router } from '../../lib/router';
+
+const api = {
+  signOut: (url) => {
+    return fetch(url).then((res) => res.json());
+  },
+  _signOut: () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ ok: true });
+      }, 800);
+    });
+  },
+};
 
 export default class Auth extends Component {
   template() {
@@ -13,12 +28,13 @@ export default class Auth extends Component {
   }
 
   mounted() {
-    const { isLogin } = this.store.states;
+    const isLogin = this.store.getState('isLogin');
     const $auth = this.$target.querySelector('.auth-wrapper');
-    if (!isLogin) {
-      new Default($auth, {}, this.store);
+    const username = this.store.getState('username');
+    if (isLogin) {
+      new Default($auth, { username }, this.store);
     } else {
-      new SignIn($auth, {}, this.store);
+      $router.push('/signin');
     }
   }
   setup() {
@@ -32,12 +48,70 @@ class Default extends Component {
   template() {
     return `
       <div class="auth-default">
+        <div class="navbar-wrapper"></div>
+        <section class="section-content">
 
+        </section>
       </div>
     `;
   }
   mounted() {
-    const $default = this.$target.querySelector('.auth-default');
-    new NavBar($default, { title: '내 계정', backGround: 'grey' }, this.store);
+    new NavBar(
+      this.$target.querySelector('.navbar-wrapper'),
+      { title: '내 계정', background: 'grey' },
+      this.store
+    );
+    new Content(this.$target.querySelector('.section-content'), {}, this.store);
+  }
+}
+
+class Content extends Component {
+  template() {
+    return `
+      <div class="username">
+      </div>
+      <div class="button-wrapper">
+      <div/>
+    `;
+  }
+  handleLogout(e) {
+    if (!e.target.closest('.button-wrapper button')) return;
+    const url = `${BASE_URL}/auth/signout`;
+
+    api._signOut(url).then((res) => {
+      if (res.ok) {
+        this.store.dispatch('setIsLogin', false);
+        $router.push('/signin');
+      }
+    });
+  }
+  mounted() {
+    this.childReRender([
+      {
+        childClass: Username,
+        selector: '.username',
+        props: { username: this.store.getState('username') || '안준범' },
+      },
+      {
+        childClass: Button,
+        selector: '.button-wrapper',
+        props: {
+          color: 'primary',
+          size: 'large',
+          text: '로그아웃',
+          rectangle: true,
+          eventTarget: '.auth-wrapper',
+          onClick: this.handleLogout.bind(this),
+        },
+      },
+    ]);
+  }
+}
+
+class Username extends Component {
+  template() {
+    return `
+        ${this.$props.username}
+    `;
   }
 }
---
+++ client/src/component/auth/utils.js
@@ -0,0 +1,41 @@
+let timeout;
+
+const inputThrottle = (callback) => {
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+  timeout = setTimeout(() => {
+    callback();
+    console.log('throttle?');
+    timeout = null;
+  }, 600);
+};
+
+const focusoutHandler = (e) => {
+  const $input = e.target.closest('.input-wrapper input');
+  if (!$input) return;
+  // const $input = document.querySelector('.input-wrapper input');
+  $input.classList.replace('active', 'initial');
+};
+
+const inputChangeHandler = (store, inputName) => (e) => {
+  const $input = e.target.closest('.input-wrapper input');
+  if (!$input) return;
+
+  // const $input = document.querySelector('.input-wrapper input');
+
+  if ($input.value.length > 0) {
+    $input.classList.replace('initial', 'active');
+  } else {
+    $input.classList.replace('active', 'initial');
+  }
+
+  const callback = () =>
+    store.dispatch('inputValue', {
+      inputName,
+      value: $input.value,
+    });
+  inputThrottle(callback);
+};
+
+export { inputChangeHandler, focusoutHandler };
